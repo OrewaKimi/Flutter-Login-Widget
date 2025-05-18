32a9142b2c69aebd8aa668fafef893cfe243b0d5
OrewaKimi <kimi93.com@gmail.com>
2025-05-18 21:23:54 +0700

    update function login,signup, and forgot password

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,15 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_login/flutter_login.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
+
+  static const loadingDuration = Duration(milliseconds: 2000);
 
   @override
   State<LoginPage> createState() => _LoginPageState();
 }
 
 class _LoginPageState extends State<LoginPage> {
+  // login
+  Future<String?> _authUser(LoginData data) {
+    return Future.delayed(LoginPage.loadingDuration).then((value) => null);
+  }
+
+  // forgot password
+  Future<String?> _recoverPassword(String email) {
+    return Future.delayed(LoginPage.loadingDuration).then((value) => null);
+  }
+
+  // sign up
+  Future<String?> _signUp(SignupData data) {
+    return Future.delayed(LoginPage.loadingDuration).then((value) => null);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: FlutterLogin(
+        title: 'My App',
+        onLogin: _authUser,
+        onRecoverPassword: _recoverPassword,
+        onSignup: _signUp,
+      ),
+    );
   }
 }
